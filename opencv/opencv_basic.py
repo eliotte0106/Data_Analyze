dc3abefcb63a3c8a931423e300933621eb2d57d9
Eliotte <72038638+eliotte0106@users.noreply.github.com>
2021-03-23 22:20:41 -0400

    Add files via upload

--- opencv/opencv_basic.py
+++ opencv/opencv_basic.py
@@ -1,1 +1,10 @@
+import cv2
 
+img_basic = cv2.imread('/Users/jihong/desktop/bella.jpg')
+cv2.imshow('Image Basic', img_basic)
+cv2.waitKey(0)
+cv2.imwrite('result1.png',img_basic)
+img_gray = cv2.cvtColor(img_basic,cv2.COLOR_BGR2GRAY)
+cv2.imshow('Image Gray', img_gray)
+cv2.waitKey(0)
+cv2.imwrite('result2.png',img_gray)
